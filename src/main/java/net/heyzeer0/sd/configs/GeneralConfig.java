5ab05684f0fc00f5f54af75f04e62b992abda998
MightyKnight <38622942+MightyKnight@users.noreply.github.com>
2022-12-09 17:14:17 +0100

    Add #c:shields tag to default disabled items
    More compatibility with mods adding new shields (if they add shields to this tag)

--- src/main/java/net/heyzeer0/sd/configs/GeneralConfig.java
+++ src/main/java/net/heyzeer0/sd/configs/GeneralConfig.java
@@ -1,21 +1,27 @@
 package net.heyzeer0.sd.configs;
 
 import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.annotation.Config;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Config(name = "shieldisruptor_general")
 public class GeneralConfig implements ConfigData {
+
+    @ConfigEntry.Gui.Excluded
+    String modVersion = "";
 
     public boolean isEnabled = true;
 
     public List<String> hiddenItems = Arrays.asList(
             Registries.ITEM.getId(Items.SHIELD).toString(),
-            Registries.ITEM.getId(Items.TOTEM_OF_UNDYING).toString());
+            Registries.ITEM.getId(Items.TOTEM_OF_UNDYING).toString(),
+            "#c:shields");
 
     // Ignore case and leading/trailing whitespaces when checking if entry is in config
     public boolean contains(String s) {
@@ -27,4 +33,21 @@ public class GeneralConfig implements ConfigData {
         return false;
     }
 
+    @Override
+    public void validatePostLoad() throws ValidationException {
+
+        // Upgrade from <=1.4.0, adding new tag "#c:shields"
+        if(modVersion == "") {
+            if(!hiddenItems.contains("#c:shields")) {
+                hiddenItems.add("#c:shields");
+            }
+            modVersion = "1.5.0";
+        }
+
+        // Remove empty list items and trim strings
+        hiddenItems.removeIf(String::isBlank);
+        hiddenItems.replaceAll(String::trim);
+
+        ConfigData.super.validatePostLoad();
+    }
 }
